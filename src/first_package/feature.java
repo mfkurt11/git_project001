dcf3e8b99ef6c13f55dc3a051fde2b34487df068
mfkurt11 <68709611+mfkurt11@users.noreply.github.com>
2020-08-08 15:51:01 -0400

    Update feature.java
    I have committed first from the remote master branch

--- src/first_package/feature.java
+++ src/first_package/feature.java
@@ -4,6 +4,8 @@ public class feature {
 
 	public static void main(String[] args) {
 		System.out.println("This is my commit from feature branch");
+		System.out.println("This is my first commit from remote master branch");
+
 
 	}
 
