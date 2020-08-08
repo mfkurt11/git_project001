8322f3db14d4ab96811ecdf414724b65651fe3d5
Fatih Kurt <mfkurt11@gmail.com>
2020-08-08 13:50:37 -0400

    My local kurt branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -4,6 +4,8 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 	System.out.println("Our first commit from local repo");
+	System.out.println("My second commit from git repository");
+	System.out.println("Now I am on my kurt branch");
 
 	}
 
